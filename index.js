57baa934901c0680c4ba93343c4f1972e3deb767
aminaly <ly.ami@husky.neu.edu>
2017-03-25 09:19:44 -0400

    Adding database

--- index.js
+++ index.js
@@ -17,4 +17,19 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+var pg = require('pg');
+
+// db page. Get's data from test table 
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('pages/db', {results: result.rows} ); }
+        });
+    });
+});
+
 
