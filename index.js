931c792665eda004aeeff529f9a0f89a55be1c85
aminaly <ly.ami@husky.neu.edu>
2017-03-25 19:16:12 -0400

    Testing queries

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
-//var indico = require('indico.io');
+var indico = require('indico.io');
 var pg = require('pg');
 var app = express();
 
-var settings = {'api_key' : "40bbea36abfef1d67b3a7befb0bf6c7c"};
+indico.apiKey =  '40bbea36abfef1d67b3a7befb0bf6c7c';
 var response = function(res) { console.log(res); }
 var logError = function(err) { console.log(err); }
+
+// operating as if first user is logged in
+var userid = 1;
 
 // local port
 app.set('port', (process.env.PORT || 5000));
@@ -28,21 +31,24 @@ app.listen(app.get('port'), function() {
 // db page. Get's data from test table
 app.get('/db', function (request, response) {
 
-    var connectionString = process.env.DATABASE_URL
-        || "postgres://localhost:5432/mr";
+    var userquery = 'SELECT * FROM users JOIN domain ON users.id = domain.user WHERE users.id = ' + userid;
+    var companyquery = 'SELECT id, domain FROM companies';
 
+    var userResult = pgquery(userquery);
+    var companyResult = pgquery(companyquery);
+    print(userResult);
+    response.render('pages/recs', {userDom: userResult, companyDom : companyResult});
+});
 
-    pg.connect(connectionString, function(err, client, done) {
-        client.query('SELECT * FROM users', function(err, result) {
+// call query
+function pgquery(querystring) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query(querystring, function(err, result) {
             done();
             if (err)
             { console.error(err); response.send("Error " + err); }
             else
-            { response.render('pages/db', {results: result.rows} ); }
+            { return(result.rows); }
         });
     });
-});
-
-// indico.sentiment(['indico is so easy to use!', 'Still really easy, yiss'], settings)
-// .then(response)
-//.catch(logError);
+}
