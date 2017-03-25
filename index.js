e8dcb8bc502da55c1255ebfbb152fcd637d2d16b
aminaly <ly.ami@husky.neu.edu>
2017-03-25 19:53:22 -0400

    pls work

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function pgquery(querystring) {
             if (err)
             { console.error(err); response.send("Error " + err); }
             else
-            { return result.rows; }
+            { return result; }
         });
     });
 }
