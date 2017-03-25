830837d035d314cfe0e9b149146df2e553475897
aminaly <ly.ami@husky.neu.edu>
2017-03-25 09:42:37 -0400

    I done fucked

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ app.listen(app.get('port'), function() {
 
 var pg = require('pg');
 
-// db page. Get's data from test table 
+// db page. Get's data from test table
 app.get('/db', function (request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function(err, result) {
+        client.query('SELECT * FROM users', function(err, result) {
             done();
             if (err)
             { console.error(err); response.send("Error " + err); }
